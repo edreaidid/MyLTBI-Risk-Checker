f1d3308c650cbe5369ea012f0f5057e25045d5cb
Edre Mohammad Aidid <61385353+edreaidid@users.noreply.github.com>
2023-10-26 10:26:20 +0800

    Update ltbisasmeclr.py

--- ltbisasmeclr.py
+++ ltbisasmeclr.py
@@ -119,7 +119,7 @@ with st.expander("Final model (FM)"):
     Y= df["LTBI"]
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import accuracy_score
-    x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.3,random_state=35)
+    x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.3,random_state=36)
     clf = LogisticRegression()
     model=clf.fit(X, Y)
     y_pred = model.predict(x_test)
